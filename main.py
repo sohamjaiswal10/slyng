4b2f7b59d01dae8aa629586d13c8ad9328757c6c
Soham Jaiswal <sohamjaiswal10@gmail.com>
2020-08-15 18:29:14 +0530

    0

---
+++ main.py
@@ -0,0 +1,12 @@
+import threading
+import tkinter as tk
+import configparser
+
+window = tk.Tk()
+window.title("Slyng")
+icon = tk.PhotoImage(file = "common\\assets\\images\\icon.gif")
+
+window.iconphoto(False, icon)
+window.mainloop()
+
+
